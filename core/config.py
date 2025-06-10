ff4a40d2b4eea10c9efa8298fdd80e96ae69fdfe
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-10 23:00:29 +0530

    Update config.py

--- core/config.py
+++ core/config.py
@@ -1,8 +1,8 @@
 # core/config.py
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-MONGO_URI = os.environ.get("MONGO_URI", "YOUR_MONGO_URI")
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "@YourLogChannel")
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "@YourUpdateChannel")
-OWNER_ID = int(os.environ.get("OWNER_ID", "123456789"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "@iPapcorn_Prime")
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "@iPapcorn_Prime")
+OWNER_ID = int(os.environ.get("OWNER_ID", "5721673207"))
