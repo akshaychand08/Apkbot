60b2fa437f21bf7180b35859e1a94c31b35b8da8
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-10 21:29:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("7823889476:AAHgG7EZ1SyhMlitrbWv2p_joHeOcTPZOYA")
+BOT_TOKEN = os.getenv("7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
 PORT = int(os.getenv("PORT", 8080))
