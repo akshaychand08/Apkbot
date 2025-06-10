8a3af6abf5bd25aef5012d34ae3a046f94972cf8
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-11 00:19:08 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,7 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_token_here")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-koyeb-app.koyeb.app/")
-MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri_here")
+#BOT_TOKEN = os.getenv("BOT_TOKEN", "your_token_here")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://growing-patricia-akshaychand12-243643d5.koyeb.app/")
+#MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri_here")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
