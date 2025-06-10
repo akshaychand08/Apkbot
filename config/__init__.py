762a5768e56f3db6252f324fc4e48b2e44ff1d4d
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-11 00:15:37 +0530

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_token_here")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-koyeb-app.koyeb.app/")
+MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri_here")
