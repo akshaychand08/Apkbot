e34716ccf3a2be5e10f6ad415e225c2b63d5537a
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-11 23:49:46 +0530

    Update mongo.py

--- core/mongo.py
+++ core/mongo.py
@@ -1,12 +1,24 @@
-from pymongo import MongoClient
 import os
+from pymongo import MongoClient
+from config import MONGO_URL
+from datetime import datetime
 
 client = None
 
 def init_db():
     global client
-    mongo_url = os.getenv("MONGO_URL", "mongodb+srv://AKirtibot:AKirtibot@cluster0.qtmdhrz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    client = MongoClient(mongo_url)
+    client = MongoClient(MONGO_URL)
 
-# Call immediately
-init_db()
+def get_user_data(user_id):
+    db = client['ai_bot']
+    user = db.users.find_one({"_id": user_id})
+    today = datetime.utcnow().date()
+    if not user or user.get('last_used') != str(today):
+        db.users.update_one({"_id": user_id}, {"$set": {"count": 0, "last_used": str(today)}}, upsert=True)
+        return {"count": 0, "last_used": str(today)}
+    return user
+
+def increment_image_count(user_id):
+    db = client['ai_bot']
+    db.users.update_one({"_id": user_id}, {"$inc": {"count": 1}})
+    
