9a0ac37871d13b9672886ad0d5ab6db4a8f9882e
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-11 23:54:23 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,9 @@
 from .start import start_cmd
-from .broadcast import broadcast_cmd
-from .admin import admin_cmd
-from .verify import verify_middleware
+from .ai_query import ai_query_handler
+from .image import image_cmd
 from telegram.ext import MessageHandler, filters
 
 def setup_handlers(app):
     app.add_handler(start_cmd)
-    app.add_handler(broadcast_cmd)
-    app.add_handler(admin_cmd)
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), verify_middleware))
-
+    app.add_handler(image_cmd)
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), ai_query_handler))
