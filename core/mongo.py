a3e52da8f1495b62002db804348f48295bd7b819
Sahid malik <106477157+Sahidmalik001@users.noreply.github.com>
2025-06-11 10:49:33 +0530

    Update mongo.py

--- core/mongo.py
+++ core/mongo.py
@@ -7,3 +7,6 @@ def init_db():
     global client
     mongo_url = os.getenv("MONGO_URL", "mongodb+srv://AKirtibot:AKirtibot@cluster0.qtmdhrz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     client = MongoClient(mongo_url)
+
+# Call immediately
+init_db()
