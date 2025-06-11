473cc173c09b3b85da4405141a9b4ef7ceb8e278
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-12 00:14:09 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,15 +1,10 @@
 import os
-
-#BOT_TOKEN = os.getenv("BOT_TOKEN", "your_token_here")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://growing-patricia-akshaychand12-243643d5.koyeb.app/")
-#MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri_here")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
-#MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
-import os
-
-#BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-#WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-koyeb-app.koyeb.app")
-OWNER_ID = os.getenv("OWNER_ID", "5721673207")
-LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "-1002138727373")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://growing-patricia-akshaychand12-243643d5.koyeb.app")
+OPENAI_KEY = os.getenv("OPENAI_KEY", "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")
+GEMINI_KEY = os.getenv("GEMINI_KEY", "AIzaSyB6idQAR_9tYkPDIJNoFUrN_Kb7ibJzz4w")
+DEEPSEEK_KEY = os.getenv("DEEPSEEK_KEY", "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")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 UPDATE_CHANNEL_ID = os.getenv("UPDATE_CHANNEL_ID", "@iPapcorn_Prime")
+LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "-1002138727373")
+IMAGE_LIMIT_PER_DAY = 5
