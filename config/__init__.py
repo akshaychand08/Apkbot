06857b31d50f06ec7801648e5ebbd4ad25c8a7ce
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-11 07:31:04 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -4,4 +4,12 @@ import os
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://growing-patricia-akshaychand12-243643d5.koyeb.app/")
 #MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_uri_here")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
-MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+#MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+import os
+
+#BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+#WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-koyeb-app.koyeb.app")
+OWNER_ID = os.getenv("OWNER_ID", "5721673207")
+LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "-1002138727373")
+UPDATE_CHANNEL_ID = os.getenv("UPDATE_CHANNEL_ID", "@iPapcorn_Prime")
