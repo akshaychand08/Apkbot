89b0375f0c5d03423c0cab5e8709d1708abe17c9
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-12 00:56:06 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -5,6 +5,6 @@ OPENAI_KEY = os.getenv("OPENAI_KEY", "sk-proj-nSu1obNmozsl3tMzBZFT9sSywlZnxrCNf6
 GEMINI_KEY = os.getenv("GEMINI_KEY", "AIzaSyB6idQAR_9tYkPDIJNoFUrN_Kb7ibJzz4w")
 DEEPSEEK_KEY = os.getenv("DEEPSEEK_KEY", "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")
 MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://iPapcorn:iPapcorn@cluster0.52lnvxn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-UPDATE_CHANNEL_ID = os.getenv("UPDATE_CHANNEL_ID", "-1002138727373")
+#UPDATE_CHANNEL_ID = os.getenv("UPDATE_CHANNEL_ID", "-1002138727373")
 LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID", "-1002138727373")
 IMAGE_LIMIT_PER_DAY = 5
