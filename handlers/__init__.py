a235f1c2c81061f1a4ad1ec181e1340fec691a00
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-12 21:30:04 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,14 @@
+from .media import media_handler, quality_handler
+from .pdf_converter import photo_to_pdf, pdf_to_photo
 from .start import start_cmd
-from telegram.ext import MessageHandler, filters
-from .ai_query import ai_query_handler
+from .stats import stats_cmd
+from .broadcast import broadcast_cmd
+from telegram.ext import MessageHandler, CommandHandler, CallbackQueryHandler, filters
 
 def setup_handlers(app):
-    for handler in start_cmd:
+    for handler in start_cmd + stats_cmd + broadcast_cmd:
         app.add_handler(handler)
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), ai_query_handler))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), media_handler))
+    app.add_handler(CallbackQueryHandler(quality_handler, pattern="^video_"))
+    app.add_handler(CommandHandler("photo2pdf", photo_to_pdf))
+    app.add_handler(CommandHandler("pdf2photo", pdf_to_photo))
