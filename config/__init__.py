40bd15dcaa42cab14a8fe43aac1164567702e62a
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-12 22:05:56 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -2,9 +2,9 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-LOG_CHANNEL_ID = os.getenv("LOG_CHANNEL_ID")
-ARCHIVE_CHANNEL_ID = os.getenv("ARCHIVE_CHANNEL_ID")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split()))
+BOT_TOKEN = os.getenv("7823889476:AAFnvcNQ1ptmshYLYhQbm2lPpkGqfb2BDZs")
+WEBHOOK_URL = os.getenv("https://growing-patricia-akshaychand12-243643d5.koyeb.app")
+LOG_CHANNEL_ID = os.getenv("-1002772843586")
+ARCHIVE_CHANNEL_ID = os.getenv("-1002772843586")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5721673207").split()))
 CREDIT = "\n\n📤 Uploaded by @iPapcorn_Prime"
