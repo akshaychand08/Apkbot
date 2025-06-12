5469802de4794b57a555f61aed1ef3232484f532
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-12 06:53:07 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,5 @@
 import os
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7823889476:AAFfu6gnn91hhlcrQdrYe2z6nqt7d9iBanc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://growing-patricia-akshaychand12-243643d5.koyeb.app")
 OPENAI_KEY = os.getenv("OPENAI_KEY", "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")
 GEMINI_KEY = os.getenv("GEMINI_KEY", "AIzaSyB6idQAR_9tYkPDIJNoFUrN_Kb7ibJzz4w")
