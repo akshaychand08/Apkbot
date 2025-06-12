c3da5b7576959416516c34c8e143bd8452a817ba
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-12 22:15:29 +0530

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-BOT_TOKEN = os.getenv("7823889476:AAFnvcNQ1ptmshYLYhQbm2lPpkGqfb2BDZs")
+BOT_TOKEN = os.getenv("7823889476:AAHNv0fkcewZQmVAmJcJVp59HzIvStZWiWo")
 WEBHOOK_URL = os.getenv("https://growing-patricia-akshaychand12-243643d5.koyeb.app")
 LOG_CHANNEL_ID = os.getenv("-1002772843586")
 ARCHIVE_CHANNEL_ID = os.getenv("-1002772843586")
