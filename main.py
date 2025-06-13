b8b5a0da53a8db2cdb49a8e01dda83f0916d64e4
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-13 06:33:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from config import BOT_TOKEN, WEBHOOK_URL
 from handlers import setup_handlers
 from core.mongo import init_db
 
-init_db()
-application = ApplicationBuilder().token(BOT_TOKEN).build()
-setup_handlers(application)
+def main():
+    init_db()
 
-if __name__ == '__main__':
-    application.run_webhook(
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    setup_handlers(app)
+
+    app.run_webhook(
         listen="0.0.0.0",
         port=8080,
         webhook_url=WEBHOOK_URL
     )
+
+if __name__ == '__main__':
+    main()
